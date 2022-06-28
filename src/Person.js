08d6b4825a20e2453172821cc6c1764495f2faf6
Gabriel-Stefan Tanul <gabriel.tanul@novcom.swiss>
2022-06-28 14:44:08 +0300

    updated image object

--- src/Person.js
+++ src/Person.js
@@ -3,8 +3,8 @@ export default function Person({props}) {
    
   return (
     <div className='flex my-5'>
-      <div>
-        <img src={props.image} className="  w-20 h-20 rounded-full shadow align-middle border-none border-gray-100"  />
+      <div >
+        <img src={props.image} className="object-cover w-20 h-20 rounded-full shadow align-middle border-none border-gray-100"  />
       </div>
       <div className="flex flex-col justify-center align-middle p-4">
         <div className=" text-slate-100"> {props.name}</div>
